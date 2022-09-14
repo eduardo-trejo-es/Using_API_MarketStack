f990520414d1c1d3b68c902e7d9f9ab367eaf768
Eduardo <trejo.alfredo.es@outlook.com>
2022-09-13 20:28:07 -0500

    saving data function

--- Testing_API.py
+++ Testing_API.py
@@ -5,21 +5,71 @@
 
 import requests
 
-params = {
-  'access_key': 'bfaf26d006f1243f13038efdfbe04ac5'
-}
+import pandas as pd
 
-#urls:http://api.marketstack.com/v1/tickers/aapl/eod
-#'http://api.marketstack.com/v1/tickers/TWTR?/eod&date_from=2022-02-24&date_to=2022-02-25'
-
-api_result = requests.get('http://api.marketstack.com/v1/tickers/TWTR/eod', params)
-
-api_response = api_result.json()
+from pathlib import Path
 
 
 
-#conn.request("GET", "/api/v1/prices/TWTR?resolution=MINUTE_10&max=10&from=2022-02-24T00:00:00&to=2022-02-24T01:00:00", payload, headers)
 
-print(api_response['data']['eod'])
 #for stock_data in api_response['data']:
 # print(u'Ticker %s has a day high of  %s on %s' % (stock_data['symbol'], stock_data['high'],stock_data['date']))
+
+
+def SavingDataPrices(From, to,csvFileName):
+  
+  HistoricalPriceRequests="http://api.marketstack.com/v1/intraday?symbols=TWTR&date_from={}&date_to={}".format(From,to)
+  
+  params = {
+    'access_key': 'bfaf26d006f1243f13038efdfbe04ac5'
+  }
+  #'http://api.marketstack.com/v1/intraday?symbols=TWTR&interval=1hour&date_from=2022-09-12 13:00:00&date_to=2022-09-13 13:00:00'
+  #date_format: 2022-09-13T00:00:00+0000
+  #url='http://api.marketstack.com/v1/tickers/aapl/eod'
+  
+
+  api_result = requests.get(HistoricalPriceRequests, params)
+
+  api_response = api_result.json()
+
+
+
+  #conn.request("GET", "/api/v1/prices/TWTR?resolution=MINUTE_10&max=10&from=2022-02-24T00:00:00&to=2022-02-24T01:00:00", payload, headers)
+  #print(api_response)
+  try:
+    for i in api_response['data']:
+      print(i)
+  except:
+    print(api_response)
+  #####      Saving Data In CSV file   ####
+
+  columnsNames=["openPrice","closePrice","highPrice","lowPrice","lastTradedVolume"]
+  DateIndexName=[]
+  columnsValues=[]
+  DataGrouped=[]
+  for i in api_response['data']:
+      DateIndexName.append(i["date"])
+      columnsValues.append(i["open"])
+      columnsValues.append(i["high"])
+      columnsValues.append(i["low"])
+      columnsValues.append(i["close"])
+      columnsValues.append(i["volume"])
+      DataGrouped.append(columnsValues)
+      columnsValues=[]
+
+  df = pd.DataFrame(DataGrouped,index=DateIndexName, columns=columnsNames)
+  
+  try:
+      existing=pd.read_csv(csvFileName, index_col="Unnamed: 0")
+      #print(existing)
+      #print(type(existing))
+      existing = existing.append(df)
+      print("was try")
+      print(existing)
+      existing.to_csv(path_or_buf=csvFileName,index=True)
+      
+  except :
+      print("was execpt")
+      df.to_csv(path_or_buf=csvFileName,index=True)
+
+SavingDataPrices("2022-08-01 13:00:00", "2022-08-29 00:00:00","TestingDataCSV.csv")
